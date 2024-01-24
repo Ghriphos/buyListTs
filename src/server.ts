8f06690d8b6d519d1345006456a99ee88a57ceb5
ghriphos <luisfernando._alves@hotmail.com>
2024-01-23 23:00:21 -0300

    feat: add routes

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import * as express from 'express'
+import { Router, Request, Response} from 'express'
+import routes from "./routes"
+
+const http = express()
+
+http.use(express.json())
+routes(http)
+
+http.listen(3000, () => { console.log(`server running on port 3000`)})
